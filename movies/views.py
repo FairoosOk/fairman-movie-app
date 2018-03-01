8d7a136bf2fadee90a0e11cf03c3743786b18a7f
Fairoos <okfairoos@gmail.com>
2018-03-01 09:13:05 +0000

    Created Django Movie App

--- movies/views.py
+++ movies/views.py
@@ -20,36 +20,43 @@ def create(request):
     if request.method == "POST":
         data={
             'Name': request.POST.get('name'),
-            'Pictures': [{'url':request.POST.get('url')}],
+            'Pictures': [{'url':request.POST.get('url') or 'http://www.clker.com/cliparts/q/L/P/Y/t/6/no-image-available-md.png'}],
             'Rating': int(request.POST.get('rating')),
+            'Trailer':request.POST.get('turl'),
             'Notes' : request.POST.get('notes')
-           }
-        response = AT.insert(data)
-
-        #notify on create
-        messages.success(request,'New Movie Added: {}'.format(response['fields'].get('Name')))
+             }
+        try:
+            response = AT.insert(data)
+            #notify on create
+            messages.success(request,'New Movie Added: {}'.format(response['fields'].get('Name')))
+        except Exception as e:
+            messages.warning(request,'Got an error:{}'.format(e))
     return redirect('/')
 
 def edit(request, movie_id):
     if request.method == "POST":
         data={
             'Name': request.POST.get('name'),
-            'Pictures': [{'url':request.POST.get('url')}],
+            'Pictures': [{'url':request.POST.get('url') or 'http://www.clker.com/cliparts/q/L/P/Y/t/6/no-image-available-md.png'}],
             'Rating': int(request.POST.get('rating')),
+            'Trailer':request.POST.get('turl'),
             'Notes': request.POST.get('notes')
              }
-
-        response = AT.update(movie_id,data)
-
-        #notify on update
-        messages.success(request,'Updated Movie: {}'.format(response['fields'].get('Name')))
+        try:
+            response = AT.update(movie_id,data)
+            #notify on update
+            messages.success(request,'Updated Movie: {}'.format(response['fields'].get('Name')))
+        except Exception as e:
+            messages.warning(request,'Got an error while trying to update a movie {}'.format(e))
     return redirect('/')
 
 def delete(request, movie_id):
-    movie_name=AT.get(movie_id)['fields'].get('Name')
-    AT.delete(movie_id)
-
-    #notify on delete
-    messages.warning(request,"Deleted Movie: {}".format(movie_name))
+    try:
+        movie_name=AT.get(movie_id)['fields'].get('Name')
+        AT.delete(movie_id)
+        #notify on delete
+        messages.warning(request,"Deleted Movie: {}".format(movie_name))
+    except Exception as e:
+        messages.warning(request,"Got an error while deleting Movie: {}".format(e))
 
     return redirect('/')
